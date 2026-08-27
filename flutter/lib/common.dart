bc1f5aacba9b86d98a8a2793f306517f3e41af87
silmarvieira-bit <silmar.vieira@protork.com>
2026-08-27 14:49:39 -0300

    Corrigir base da personalização visual

--- flutter/lib/common.dart
+++ flutter/lib/common.dart
@@ -84,6 +84,8 @@ const double _kPositionEpsilon = 1e-6;
 bool get isMainDesktopWindow =>
     desktopType == DesktopType.main || desktopType == DesktopType.cm;
 
+String get screenInfo => screenInfo_;
+
 /// Check if the app is running with single view mode.
 bool isSingleViewApp() {
   return desktopType == DesktopType.cm;
